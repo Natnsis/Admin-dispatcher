74198b8b595b57bc1b9c6ff9970abdb2d1616104
natnael <nsisay49@gmail.com>
2025-09-01 19:12:14 +0300

    vite with shadcn initialized

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className="bg-amber-200">
+      hello wolrd
+      <div>
+        <Button>bahahah</Button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
